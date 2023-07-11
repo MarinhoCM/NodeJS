19df8397f0320a645eccf9dcbca2688945b637d6
MarinhoCM <marinhoc384@gmail.com>
2023-07-10 23:01:13 -0300

    :truck:build: rename index.js to menorValor.js

---
+++ js_algorithms/menorValor.js
@@ -0,0 +1,10 @@
+function menorValor(arrProdutos, posicaoInicial){
+    let maisBarato = posicaoInicial;
+    for (let i = 0; i <= arrProdutos.length; i++) {
+        if(arrProdutos[i].preco < arrProdutos[maisBarato].preco){
+            maisBarato = i;
+        }
+    }
+    return maisBarato;
+}
+module.exports = menorValor;
---
+++ js_algorithms/selectionSort.js
@@ -0,0 +1,6 @@
+const livros = require('./listaLivros')
+const menorValor = require('./menorValor')
+
+for (let i = 0; i < livros.length; i++) {
+    if (livros[i].preco < )
+}
