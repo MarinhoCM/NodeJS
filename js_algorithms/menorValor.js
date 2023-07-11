6beb7d9aef86f7c5535066b1b3f6520bc849961d
MarinhoCM <marinhoc384@gmail.com>
2023-07-10 23:16:11 -0300

    :sparkles:feat: update repo add selectionSort file

--- js_algorithms/menorValor.js
+++ js_algorithms/menorValor.js
@@ -1,3 +1,4 @@
+const livros = require('./listaLivros')
 function menorValor(arrProdutos, posicaoInicial){
     let maisBarato = posicaoInicial;
     for (let i = 0; i <= arrProdutos.length; i++) {
--- js_algorithms/selectionSort.js
+++ js_algorithms/selectionSort.js
@@ -2,5 +2,10 @@ const livros = require('./listaLivros')
 const menorValor = require('./menorValor')
 
 for (let i = 0; i < livros.length; i++) {
-    if (livros[i].preco < )
+    let menor = menorValor(livros, i);
+    let livroAtual = livros[i];
+    let livroMenorPreco = livros[menor];
+    livros[i] = livroMenorPreco;
+    livros[menor] = livroAtual;
 }
+console.log(livros)
