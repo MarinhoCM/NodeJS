a94aa647020ff49000e0271b2ea5d487611867b0
MarinhoCM <cauamarinhodesousa@gmail.com>
2023-10-23 18:24:08 -0300

    :sparkles: feat: add reviews folder to repo

---
+++ reviews/challenge006.js
@@ -0,0 +1,28 @@
+// var championship = 'Campeonato Paulista';
+// console.log(championship);
+
+// let teams = ["São Paulo", "Paumeiras", "Santos", "Corithians", "Ponte Preta"];
+// console.log(teams)
+
+
+// function showTeamPosition(position){
+//     if(position < 1 || position > 5){
+//         return "Não temos informação do time que está nessa posição."
+//     } else {
+//         return `O time que esta em ${position} lugar é o ${teams[position + 1]}.`
+//     }
+// };
+
+// console.log(showTeamPosition(0))
+// console.log(showTeamPosition(1))
+// console.log(showTeamPosition(2))
+// console.log(showTeamPosition(3))
+// console.log(showTeamPosition(4))
+// console.log(showTeamPosition(5))
+// console.log(showTeamPosition(6))
+
+let num = 20;
+
+while(num <= 30){
+    console.log(num++);
+}
