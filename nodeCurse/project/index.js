de183c708bef713714eb0f6de8104986c64edad8
MarinhoCM <marinhoc384@gmail.com>
2023-11-02 22:21:15 -0300

    :sparkles: feat: adicionando rota 'perguntar'

--- nodeCurse/project/index.js
+++ nodeCurse/project/index.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const app = express();
+const srvPort = 4000;
 
-app.get("/",function (req, res){
-    res.send("<h1>Welcome to my landing-page!</h1>")
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("index");
 });
-app.get("/blog", function (req, res){
-   res.send("<h1>Welcome to my blog!</h1>")
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
 });
-app.get("/channel", function (req, res){
-   res.send("<h1>Welcome to my channel!</h1>")
-});
-app.get("/about/:number/", function(req, res){
-    let loop = function(number = 10){
-        for(let i = 0; i <= number; i++){
-            console.log(`Count: ${i} to ${number}`);
-        };
-        return "loop run out"
-    };
-    let message  = loop(req.params.number);
-    res.send(`<h3><b>${message} to ${req.params.number}<b></h3>`);
-});
-app.get('/hello/:name/:company', function (req, res){
-    res.send(`<h1>Hello!, ${req.params.name} from the company Normalabs.</h1>`)
-})
-app.listen(4000,function (err){
-    if(err){
-        console.log("An error occurred while starting the server!");
+
+app.listen(srvPort, (err) => {
+    if (err) {
+        console.log(
+            `An error occurred when trying to make the service available\n` ,
+            "error:", err
+        );
     } else {
-        console.log("server successfully started");
-    }
-})
+        console.log(
+            "#################################################\n" +
+            `\tthe server is running on: ${srvPort}\n`
+            + "#################################################"
+        );
+    };
+});
